cf1bc02cfc421a8e80300578745f605c501f07ac
Nishant Krishna <167810171+nishant00155@users.noreply.github.com>
2026-05-07 19:55:28 +0530

    Time: 7 ms (99.05%) | Memory: 24.9 MB (78.61%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>ans;
+        unordered_map<string,vector<string>>map;
+        for(auto &x:strs){
+            string temp=x;
+            sort(x.begin(),x.end());
+            map[x].push_back(temp);
+        }
+
+        for(auto &y:map){
+            ans.push_back(y.second);
+        }
+        return ans;
+    }
+};
